e71a0591627698c06851dc3c77986d58fb2d3e99
blackknight75 <devbox4714@gmail.com>
2017-04-13 09:27:42 -0600

    add endpoint for single item

--- spec/requests/api/request_spec.rb
+++ spec/requests/api/request_spec.rb
@@ -13,6 +13,18 @@ describe "Items", type: :request do
   items = JSON.parse(response.body, symbolize_names: true)
   expect(items.count).to eq 3
   expect(items.first[:name]).to eq("stuff")
-  expect(items.last).to eq("again stuff")
+  expect(items.last[:name]).to eq("again stuff")
+  end
+
+  it "returns a single item" do
+    item = Item.create(name: "stuff", description: "some stuff", image_url: "https://placehold.it/300x300.png/000")
+
+  get "/api/v1/items/#{item.id}"
+
+  expect(response).to be_success
+
+  item = JSON.parse(response.body, symbolize_names: true)
+  expect(item[:name]).to eq("stuff")
+  expect(item[:description]).to eq("some stuff")
   end
 end
