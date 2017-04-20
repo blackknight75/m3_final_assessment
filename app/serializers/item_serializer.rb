bdcba352c5df0fa0f9f1e40f13ab0283c14096d2
blackknight75 <devbox4714@gmail.com>
2017-04-20 14:11:42 -0600

    rspec passing

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,3 +1,7 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :image_url
+
+  has_many :order_items
+  has_many :orders, through: :order_items
+
 end
--- spec/requests/api/request_spec.rb
+++ spec/requests/api/request_spec.rb
@@ -11,21 +11,21 @@ describe "Items", type: :request do
   expect(response).to be_success
 
   items = JSON.parse(response.body, symbolize_names: true)
-  expect(items.count).to eq 3
-  expect(items.first[:name]).to eq("stuff")
-  expect(items.last[:name]).to eq("again stuff")
+
+  expect(items[:items].count).to eq 3
+  expect(items[:items].first[:name]).to eq("stuff")
+  expect(items[:items].last[:name]).to eq("again stuff")
   end
 
   it "returns a single item" do
     item = Item.create(name: "stuff", description: "some stuff", image_url: "https://placehold.it/300x300.png/000")
 
-  get "/api/v1/items/#{item.id}"
+    get "/api/v1/items/#{item.id}"
+    expect(response).to be_success
 
-  expect(response).to be_success
-
-  item = JSON.parse(response.body, symbolize_names: true)
-  expect(item[:name]).to eq("stuff")
-  expect(item[:description]).to eq("some stuff")
+    item = JSON.parse(response.body, symbolize_names: true)
+    expect(item[:item][:name]).to eq("stuff")
+    expect(item[:item][:description]).to eq("some stuff")
   end
 
   it "deletes a single item" do
